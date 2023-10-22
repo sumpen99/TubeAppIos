7cebace9443540975e650e1498faab4a26f5e28c
sumpen99 <sumpen841001@hotmail.com>
2023-10-22 21:14:43 +0200

    toast and bug fixes

--- TubePipe/View/Pages/Home/Document/ShareDocumentView.swift
+++ TubePipe/View/Pages/Home/Document/ShareDocumentView.swift
@@ -120,33 +120,6 @@ struct ShareDocumentView:View{
         .padding([.leading,.trailing])
     }
     
-    /*
-     ack(spacing: 32) {
-           Button {
-             toast = Toast(style: .success, message: "Saved.", width: 160)
-           } label: {
-             Text("Run (Success)")
-           }
-           
-           Button {
-             toast = Toast(style: .info, message: "Btw, you are a good person.")
-           } label: {
-             Text("Run (Info)")
-           }
-           
-           Button {
-             toast = Toast(style: .warning, message: "Beware of a dog!")
-           } label: {
-             Text("Run (Warning)")
-           }
-           
-           Button {
-             toast = Toast(style: .error, message: "Fatal error, blue screen level.")
-           } label: {
-             Text("Run (Error)")
-           }
-     
-     */
     var body: some View{
         VStack(spacing:0){
             TopMenu(title: "Share document", actionCloseButton: closeView)
